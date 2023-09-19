35741753bc131664ebeacb91fac48e80a55a2cb7
rowanhnwl <rowan.honeywell@mail.utoronto.ca>
2023-09-19 13:36:08 -0400

    Implemented the full navbar functionality

--- src/hello.py
+++ src/hello.py
@@ -1,8 +1,12 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+
+from datetime import datetime
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 @app.route('/')
 def index():
